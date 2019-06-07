6117014b96acc983e90f6f97a990a75f95980ed1
violettebec <bechoerner@gmail.com>
2019-06-07 11:24:17 -0500

    updating projects

--- js/init.js
+++ js/init.js
@@ -3,7 +3,11 @@
   
       $('.sidenav').sidenav();
       $('.parallax').parallax();
+      $('.collapsible').collapsible();
 
   
     }); // end of document ready
   })(jQuery); // end of jQuery name space
+
+  
+
